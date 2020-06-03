a94b8a44083a2fb13fc08facc9e208577d747927
sari huminer <sh6197174@gmail.com>
2020-06-03 17:56:25 +0300

    find the winner Party

--- party.py
+++ party.py
@@ -1,6 +1,26 @@
+import numpy
+import db
+
+
 class Party:
-    def __init__(self,kod,name,char):
-        self.kod=kod
-        self.name=name
-        self.char=char
-        self.countVoters=0
+    def __init__(self, kod, name, char):
+        self.kod = kod
+        self.name = name
+        self.char = char
+        self.countVoters = 0
+
+    def __init__(self, ):
+        pass
+
+    def get_max_Party(self):
+        count = db.cursor.execute(
+            "select [nameParty],[charParty] from [dbo].[Party] where[countVoters] = (select   max([countVoters]) from [dbo].[Party])"
+        ).fetchone()
+
+        print("המפלגה עם מספר הקולות הגבוה ביותר הינה {} - {}".format(count[1], count[0]))
+        # numpy.array()
+
+
+if __name__ == '__main__':
+    p = Party()
+    p.get_max_Party()
